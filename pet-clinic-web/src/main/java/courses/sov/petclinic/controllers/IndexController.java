796ad1096621e62b3472c9d19049f07e65754a81
Danilo Cividin <danilo.cividin.ext@infocert.it>
2021-05-20 16:35:59 +0200

    add PetClinic index. closes #7

---
+++ pet-clinic-web/src/main/java/courses/sov/petclinic/controllers/IndexController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package courses.sov.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author dcividin
+ *
+ */
+@Controller
+public class IndexController {
+
+	@RequestMapping({"", "/", "index", "index.html"})
+	public String index(Model model) {
+		return "index";
+	}
+}
