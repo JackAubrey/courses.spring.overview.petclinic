75aab9320cfa34dd5339ccbd3b0899eb39b3a1c7
Danilo Cividin <danilo.cividin.ext@infocert.it>
2021-06-16 14:46:59 +0200

    Created Specialty map Service impl. Closes #33

---
+++ pet-clinic-data/src/main/java/courses/sov/petclinic/service/SpecialtyService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package courses.sov.petclinic.service;
+
+import courses.sov.petclinic.model.Specialty;
+
+/**
+ * @author dcividin
+ *
+ */
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/courses/sov/petclinic/service/map/SpecialtyServiceMap.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package courses.sov.petclinic.service.map;
+
+import org.springframework.stereotype.Service;
+
+import courses.sov.petclinic.model.Specialty;
+import courses.sov.petclinic.service.SpecialtyService;
+
+/**
+ * @author dcividin
+ *
+ */
+@Service
+public class SpecialtyServiceMap extends AbstractMapCrudService<Specialty, Long> implements SpecialtyService {
+
+
+}
