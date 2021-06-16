bf59572882fe9d7aaa4de6035d33fbf22cce02c8
Danilo Cividin <danilo.cividin.ext@infocert.it>
2021-06-16 11:59:28 +0200

    Added Contact properties associated to Owner. Closes #30

--- pet-clinic-data/src/main/java/courses/sov/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/courses/sov/petclinic/model/Owner.java
@@ -14,16 +14,54 @@ public class Owner extends Person {
 	 * 
 	 */
 	private static final long serialVersionUID = 3102221271274397930L;
-
+	
+	private String address;
+	private String city;
+	private String telephone;
 	private Set<Pet> pets;
-
+	
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	/**
+	 * @return the telephone
+	 */
+	public String getTelephone() {
+		return telephone;
+	}
+	/**
+	 * @param telephone the telephone to set
+	 */
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 	/**
 	 * @return the pets
 	 */
 	public Set<Pet> getPets() {
 		return pets;
 	}
-
 	/**
 	 * @param pets the pets to set
 	 */
