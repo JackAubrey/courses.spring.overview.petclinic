df706cd184e9a4840778654ab7d98c1a4af1ac52
Danilo Cividin <danilo.cividin.ext@infocert.it>
2021-07-01 17:05:47 +0200

    Create Visit Map Service. Closes #48

---
+++ pet-clinic-data/src/main/java/courses/sov/petclinic/service/VisitService.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package courses.sov.petclinic.service;
+
+import courses.sov.petclinic.model.Visit;
+
+/**
+ * @author dcividin
+ *
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/courses/sov/petclinic/service/map/VisitMapService.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package courses.sov.petclinic.service.map;
+
+import org.springframework.stereotype.Service;
+
+import courses.sov.petclinic.model.Visit;
+import courses.sov.petclinic.service.VisitService;
+
+/**
+ * @author dcividin
+ *
+ */
+@Service
+public class VisitMapService extends AbstractMapCrudService<Visit, Long> implements VisitService {
+
+	@Override
+	public Visit save(Visit visit) {
+		if(visit == null || visit.getPet() == null || visit.getPet().getOwner() == null
+				|| visit.getPet().getId() == null || visit.getPet().getId() < 1
+				|| visit.getPet().getOwner().getId() == null || visit.getPet().getOwner().getId() < 1) {
+			throw new IllegalArgumentException("Visit has no pet or owner");
+		}
+		
+		return super.save(visit);
+	}
+}
