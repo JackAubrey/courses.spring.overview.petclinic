afa0f99d9871c8c585ddcda15b019923903e0df9
Danilo Cividin <danilo.cividin.ext@infocert.it>
2021-07-01 10:36:37 +0200

    missing class of #41

--- pet-clinic-data/src/main/java/courses/sov/petclinic/repositories/VisitRepository.java
+++ pet-clinic-data/src/main/java/courses/sov/petclinic/repositories/VisitRepository.java
@@ -5,12 +5,12 @@ package courses.sov.petclinic.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
-import courses.sov.petclinic.model.Vet;
+import courses.sov.petclinic.model.Visit;
 
 /**
  * @author dcividin
  *
  */
-public interface VisitRepository extends CrudRepository<Vet, Long> {
+public interface VisitRepository extends CrudRepository<Visit, Long> {
 
 }
